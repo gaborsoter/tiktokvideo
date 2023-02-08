8ecec002f25aa21e2481833b1261cc5f84b25cf3
gaborsoter <sotergabor@gmail.com>
2023-02-08 22:13:55 +0000

    add file upload

--- streamlit-app/streamlit_app.py
+++ streamlit-app/streamlit_app.py
@@ -4,6 +4,7 @@ import requests
 import json
 from pydub import AudioSegment
 import os
+import boto3
 
 def export_audio(uploaded_file):
     str_one = "ffmpeg -i "
@@ -18,7 +19,24 @@ if uploaded_file is not None:
     file_details = {"FileName":uploaded_file.name,"FileType":uploaded_file.type,"FileSize":uploaded_file.size}
     st.write(file_details)
 
-    export_audio(uploaded_file)
+    session = boto3.session.Session()
+    client = session.client('s3',
+                            endpoint_url='https://tenxshorts.ams3.digitaloceanspaces.com', # Find your endpoint in the control panel, under Settings. Prepend "https://".
+                            config=botocore.config.Config(s3={'addressing_style': 'virtual'}), # Configures to use subdomain/virtual calling format.
+                            region_name='ams3', # Use the region in your endpoint.
+                            aws_access_key_id='DO00AEDRKJUZK2F7V2DZ', # Access key pair. You can create access key pairs using the control panel or API.
+                            aws_secret_access_key=os.getenv('SPACES_KEY')) # Secret access key defined through an environment variable.
+
+    # Step 3: Call the put_object command and specify the file to upload.
+    client.put_object(Bucket='tenxshorts', # The path to the directory you want to upload the object to, starting with your Space name.
+                    Key=uploaded_file, # Object key, referenced whenever you want to access this file later.
+                    ACL='private', # Defines Access-control List (ACL) permissions, such as private or public.
+                    Metadata={ # Defines metadata tags.
+                        'x-amz-meta-my-key': 'your-value'
+                    }
+                    )
+
+    #export_audio(uploaded_file)
 
     #sound = AudioSegment.from_wav("input.mp4.wav")
     #sound = sound.set_channels(1)
@@ -28,4 +46,3 @@ if uploaded_file is not None:
     #    os.remove("input.mp4.wav")
     #except:
     #    pass
-

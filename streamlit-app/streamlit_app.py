e4e345e79252674c01717c3f3a4bb5e8bf14ae6f
gaborsoter <sotergabor@gmail.com>
2023-02-08 20:52:08 +0000

    add file uploader

--- streamlit-app/streamlit_app.py
+++ streamlit-app/streamlit_app.py
@@ -2,3 +2,8 @@ import streamlit as st
 import pandas as pd
 
 st.write("Hello world") 
+
+uploaded_file = st.file_uploader("Upload Files",type=['mp4'])
+if uploaded_file is not None:
+    file_details = {"FileName":uploaded_file.name,"FileType":uploaded_file.type,"FileSize":uploaded_file.size}
+     st.write(file_details)

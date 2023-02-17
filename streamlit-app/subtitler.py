c0acfe24f49e7d3f1e48a04ef05bd493a277c73a
gaborsoter <sotergabor@gmail.com>
2023-02-17 13:50:02 +0000

    fix

--- streamlit-app/subtitler.py
+++ streamlit-app/subtitler.py
@@ -30,81 +30,6 @@ class Subtitler:
         return
 
     def __call__(self, audio):
-        #model = whisper.load_model("large-v2")
-        #audio = whisper.load_audio(audio)
-        #transcription = model.transcribe(audio)
-        
-        # convert audio to binary data
-        audio = base64.b64encode(audio).decode("utf-8")
-
-        print("HERE")
-
-        inputs = {
-            # Audio file
-            'audio': "https://ams3.digitaloceanspaces.com/tenxshorts/210016557725152843431569.wav?X-Amz-Algorithm=AWS4-HMAC-SHA256&X-Amz-Credential=DO00MPAQNTRMXVXUEJUX%2F20230217%2Fams3%2Fs3%2Faws4_request&X-Amz-Date=20230217T130940Z&X-Amz-Expires=21600&X-Amz-SignedHeaders=host&X-Amz-Signature=304fcfb7a73ca4d993d6c12eb54b0ac188153b6842a14664d2c9a33799532056",
-
-            # Choose a Whisper model.
-            'model': "base",
-
-            # Choose the format for the transcription
-            'transcription': "plain text",
-
-            # Translate the text to English when set to True
-            'translate': False,
-
-            # temperature to use for sampling
-            'temperature': 0,
-
-            # comma-separated list of token ids to suppress during sampling; '-1'
-            # will suppress most special characters except common punctuations
-            'suppress_tokens': "-1",
-            'condition_on_previous_text': True,
-            'temperature_increment_on_fallback': 0.2,
-            'compression_ratio_threshold': 2.4,
-            'logprob_threshold': -1,
-            'no_speech_threshold': 0.6,
-        }
-
-        # https://replicate.com/openai/whisper/versions/30414ee7c4fffc37e260fcab7842b5be470b9b840f2b608f5baa9bbef9a259ed#output-schema
-        transcription = version.predict(**inputs)
-
-        st.write(transcription)
-
-        url = "https://ams3.digitaloceanspaces.com/tenxshorts/210016557725152843431569.wav?X-Amz-Algorithm=AWS4-HMAC-SHA256&X-Amz-Credential=DO00MPAQNTRMXVXUEJUX%2F20230217%2Fams3%2Fs3%2Faws4_request&X-Amz-Date=20230217T130940Z&X-Amz-Expires=21600&X-Amz-SignedHeaders=host&X-Amz-Signature=304fcfb7a73ca4d993d6c12eb54b0ac188153b6842a14664d2c9a33799532056"
-        audio = urlopen(url).read()
-
-        # print the recognized text
-        segments = transcription["segments"]
-        start_index = 0
-        total_subs = []
-        for i,segment in enumerate(segments):
-            text = segment["text"]
-            audioSegment = AudioSegment.from_wav(BytesIO(audio))[segment["start"]*1000:segment["end"]*1000]
-            audioSegment.export(str(i)+'.wav', format="wav") #Exports to a wav file in the current path.
-            transcript=text.strip().replace(" ", "|")
-            transcript = re.sub(r'[^\w|\s]', '', transcript)
-            transcript = re.sub(r"(\d+)", lambda x: num2words.num2words(int(x.group(0))), transcript)
-            subs = force_align(str(i)+'.wav', transcript.upper(), start_index, segment["start"])
-            start_index += len(segment["text"])
-            total_subs.extend(subs)
-        
-        st.write(total_subs)
-
-        #CAPTION_FILE = open("files/caption.srt", "w")
-        #CAPTION_FILE.write(compose(total_subs))
-        #CAPTION_FILE.close()
-
-        #TRANSCRIPT = open("files/transcript.txt", "w")
-        #TRANSCRIPT.write(transcription["text"])
-        #TRANSCRIPT.close()
-
-        #try:
-        #    for i in range(50):
-        #        os.remove(str(i)+".wav")
-        #except:
-        #    pass
-        return 
-
         def force_align(SPEECH_FILE, transcript, start_index, start_time):
             bundle = torchaudio.pipelines.WAV2VEC2_ASR_BASE_960H
             model = bundle.get_model().to(device)
@@ -257,3 +182,79 @@ class Subtitler:
                 subs.append(subtitle)
 
             return subs
+        #model = whisper.load_model("large-v2")
+        #audio = whisper.load_audio(audio)
+        #transcription = model.transcribe(audio)
+        
+        # convert audio to binary data
+        audio = base64.b64encode(audio).decode("utf-8")
+
+        print("HERE")
+
+        inputs = {
+            # Audio file
+            'audio': "https://ams3.digitaloceanspaces.com/tenxshorts/210016557725152843431569.wav?X-Amz-Algorithm=AWS4-HMAC-SHA256&X-Amz-Credential=DO00MPAQNTRMXVXUEJUX%2F20230217%2Fams3%2Fs3%2Faws4_request&X-Amz-Date=20230217T130940Z&X-Amz-Expires=21600&X-Amz-SignedHeaders=host&X-Amz-Signature=304fcfb7a73ca4d993d6c12eb54b0ac188153b6842a14664d2c9a33799532056",
+
+            # Choose a Whisper model.
+            'model': "base",
+
+            # Choose the format for the transcription
+            'transcription': "plain text",
+
+            # Translate the text to English when set to True
+            'translate': False,
+
+            # temperature to use for sampling
+            'temperature': 0,
+
+            # comma-separated list of token ids to suppress during sampling; '-1'
+            # will suppress most special characters except common punctuations
+            'suppress_tokens': "-1",
+            'condition_on_previous_text': True,
+            'temperature_increment_on_fallback': 0.2,
+            'compression_ratio_threshold': 2.4,
+            'logprob_threshold': -1,
+            'no_speech_threshold': 0.6,
+        }
+
+        # https://replicate.com/openai/whisper/versions/30414ee7c4fffc37e260fcab7842b5be470b9b840f2b608f5baa9bbef9a259ed#output-schema
+        transcription = version.predict(**inputs)
+
+        st.write(transcription)
+
+        url = "https://ams3.digitaloceanspaces.com/tenxshorts/210016557725152843431569.wav?X-Amz-Algorithm=AWS4-HMAC-SHA256&X-Amz-Credential=DO00MPAQNTRMXVXUEJUX%2F20230217%2Fams3%2Fs3%2Faws4_request&X-Amz-Date=20230217T130940Z&X-Amz-Expires=21600&X-Amz-SignedHeaders=host&X-Amz-Signature=304fcfb7a73ca4d993d6c12eb54b0ac188153b6842a14664d2c9a33799532056"
+        audio = urlopen(url).read()
+
+        # print the recognized text
+        segments = transcription["segments"]
+        start_index = 0
+        total_subs = []
+        for i,segment in enumerate(segments):
+            text = segment["text"]
+            audioSegment = AudioSegment.from_wav(BytesIO(audio))[segment["start"]*1000:segment["end"]*1000]
+            audioSegment.export(str(i)+'.wav', format="wav") #Exports to a wav file in the current path.
+            transcript=text.strip().replace(" ", "|")
+            transcript = re.sub(r'[^\w|\s]', '', transcript)
+            transcript = re.sub(r"(\d+)", lambda x: num2words.num2words(int(x.group(0))), transcript)
+            subs = force_align(str(i)+'.wav', transcript.upper(), start_index, segment["start"])
+            start_index += len(segment["text"])
+            total_subs.extend(subs)
+        
+        st.write(total_subs)
+
+        #CAPTION_FILE = open("files/caption.srt", "w")
+        #CAPTION_FILE.write(compose(total_subs))
+        #CAPTION_FILE.close()
+
+        #TRANSCRIPT = open("files/transcript.txt", "w")
+        #TRANSCRIPT.write(transcription["text"])
+        #TRANSCRIPT.close()
+
+        #try:
+        #    for i in range(50):
+        #        os.remove(str(i)+".wav")
+        #except:
+        #    pass
+        return 
+
+        
